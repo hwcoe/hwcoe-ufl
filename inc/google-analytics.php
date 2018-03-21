863a73be900d1fc28fc998ffb9af0f260dcb1f22
sezj <sarahzachrich@gmail.com>
2018-03-21 16:08:48 -0400

    Update Google Analytics script to gtag.js

--- inc/google-analytics.php
+++ inc/google-analytics.php
@@ -2,14 +2,16 @@
 	$opt_analytics_acct = get_theme_mod( 'analytics_acct', false );
 	if ( $opt_analytics_acct && !is_user_logged_in () ):
 ?>
-<!-- Google Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '<?php echo $opt_analytics_acct; ?>', 'auto');
-  ga('send', 'pageview');
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '<?php echo $opt_analytics_acct; ?>');
 </script>
+
+
 <?php endif; ?>
