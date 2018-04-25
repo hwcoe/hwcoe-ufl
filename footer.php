9173ded9b9de55541da098ee070db36043177249
sezj <sarahzachrich@gmail.com>
2018-04-25 16:23:38 -0400

    Fix mobile styles for footer buttons; integrate ACF footer options

--- footer.php
+++ footer.php
@@ -30,9 +30,17 @@
 					</ul>
                     <?php get_sidebar('footer_right'); ?>
 				</div>
-
 				<div class="col-md-8 col-md-pull-4">
-					<?php get_sidebar('site_footer'); ?>
+					<?php if ( get_field( 'override_footer_columns', 'option' ) ) : ?>
+						<?php while ( have_rows( 'footer_columns', 'option' ) ) : the_row(); ?>
+							<div class="col-md-<?php the_sub_field( 'columns' ); ?> col-sm-<?php the_sub_field( 'columns' ); ?> footer-menu">
+								<h2><?php the_sub_field( 'heading' ); ?> <span class="icon-svg icon-caret"><svg><use xlink:href="<?php echo HWCOE_UFL_IMG_DIR; ?>/spritemap.svg#caret"></use></svg></span></h2>
+								<?php the_sub_field( 'list_items' ); ?>
+							</div>
+						<?php endwhile // footer_columns ?>
+					<?php else: ?>
+						<?php get_sidebar('site_footer'); ?>
+					<?php endif //override_footer_columns ?>
 				</div>
 			</div><!-- .row -->
 		</div><!-- .container --> 
