05dcece6b73e63d90e93c4f08bfa24843fec184f
sezj <sarahzachrich@gmail.com>
2018-05-22 11:45:56 -0400

    add $content_type (twitter, catlist, story) to secondary featured stories module

--- inc/modules/ufl-secondary-featured.php
+++ inc/modules/ufl-secondary-featured.php
@@ -56,61 +56,68 @@
 ?>
 <?php if( have_rows('content_2') ): ?>
 	<?php while ( have_rows('content_2') ) : the_row(); ?> 
-		<?php if( get_sub_field( 'content_enable' ) ): ?>
+		<?php $content_type = get_sub_field( 'content_type' ); ?>
+		<?php if ( $content_type === "post_list" ): ?>
 			<div class="secondary-featured-block two in-bottom"> 
+				<?php $story_category = get_sub_field( 'story_category' ); ?>
 				<div class="secondary-featured-story <?php if( get_sub_field('background_gradient') ){ echo "gradient-bg"; } ?>">
 					<div class="secondary-featured-copy">
-						<?php the_sub_field( 'open_content' ); ?>
+						<?php esc_attr( the_sub_field( 'post_list_shortcode' ) ); ?>
 					</div>
+					<a href="<?php echo get_category_link( $story_category ); ?>" class="category-tag"><?php echo get_cat_name( $story_category ); ?></a>
 				</div>
 			</div>
-		<?php elseif ( get_sub_field( 'twitter_widget' ) ): ?>
+		<?php elseif ( $content_type === "twitter_widget" ): ?>
 			<div class="secondary-featured-block two in-bottom" style="background: transparent;">
 				<div class="tweet-block" style="background: transparent;">
-					<?php the_sub_field( 'twitter_embed_code' ); ?>
+					<?php esc_attr( the_sub_field( 'twitter_embed_code' ) ); ?>
 				</div>
-			</div>
+			</div>	
 		<?php else: ?>
 			<div class="secondary-featured-block two in-bottom"> 
-		<?php $story_category = get_sub_field( 'story_category' ); ?>
-		<?php if( get_sub_field( 'pull_latest_from_category' ) ): ?>
-			<?php        
-				$args = array(
-					'posts_per_page' => 1,
-					'category' => $story_category,
-					'orderby' => 'date',
-					'order' => 'DESC',
-					'post_type' => 'post',
-					'post_status' => 'publish',
-				);
-				$recent_posts = get_posts( $args );
-				$secondary_story = $recent_posts[0];
-				wp_reset_postdata();
-				$excerpt = ( $secondary_story->post_excerpt ? $secondary_story->post_excerpt : hwcoe_ufl_trim_content( $secondary_story->post_content, 135, '...' ));
-			?>
-			<?php $story_background = (get_sub_field( 'story_background_image') ? get_sub_field( 'story_background_image' ) : wp_get_attachment_url( get_post_thumbnail_id( $secondary_story->ID ) ) ); ?>
+			<?php $story_category = get_sub_field( 'story_category' ); ?>
+			<?php if( get_sub_field( 'pull_latest_from_category' ) ): ?>
+				<?php        
+					$args = array(
+						'posts_per_page' => 1,
+						'category' => $story_category,
+						'orderby' => 'date',
+						'order' => 'DESC',
+						'post_type' => 'post',
+						'post_status' => 'publish',
+					);
+					$recent_posts = get_posts( $args );
+					$secondary_story = $recent_posts[0];
+					wp_reset_postdata();
+					$excerpt = ( $secondary_story->post_excerpt ? $secondary_story->post_excerpt : hwcoe_ufl_trim_content( $secondary_story->post_content, 135, '...' ));
+				?>
+				
+				<?php 
+					// if story_background_image is specified then use it, otherwise use post thumbnail 
+					$story_background = (get_sub_field( 'story_background_image') ? get_sub_field( 'story_background_image' ) : wp_get_attachment_url( get_post_thumbnail_id( $secondary_story->ID ) ) ); ?>
 				<div class="secondary-featured-story <?php if( get_sub_field('background_gradient') ){ echo "gradient-bg"; } ?>" <?php if( $story_background ){ echo "style='background-image:url(" . $story_background . ")'"; } ?>>
 					<div class="secondary-featured-copy">
+
 						<h2><a href="<?php echo esc_url( get_permalink( $secondary_story->ID ) ); ?>"><?php echo get_the_title( $secondary_story->ID ); ?></a></h2>
-						<p><?php echo $excerpt; ?></p>
+						<p><?php echo $excerpt ?></p>
 						<a href="<?php echo esc_url( get_permalink( $secondary_story->ID ) ); ?>" class="read-more">Read more</a>
 					</div>
 					<a href="<?php echo get_category_link( $story_category ); ?>" class="category-tag"><?php echo get_cat_name( $story_category ); ?></a>
 				</div>
+
 		<?php else: ?>
 			<?php $story_background = get_sub_field( 'story_background_image'); ?>
-			<div class="secondary-featured-story <?php if( get_sub_field('background_gradient') ){ echo "gradient-bg"; } ?>" <?php if( $story_background ){ echo "style='background-image:url(" . $story_background . ")'"; } ?>>
+				<div class="secondary-featured-story <?php if( get_sub_field('background_gradient') ){ echo "gradient-bg"; } ?>" <?php if( $story_background ){ echo "style='background-image:url(" . $story_background . ")'"; } ?>>
 				<div class="secondary-featured-copy">
 					<h2><a href="<?php esc_url( the_sub_field( 'story_link' ) ); ?>"><?php the_sub_field( 'story_title' ) ?></a></h2>
 					<p><?php the_sub_field( 'story_excerpt' ); ?></p>
 					<a href="<?php esc_url( the_sub_field( 'story_link' ) ); ?>" class="read-more">Read more</a>
 				</div>
-				<a href="<?php echo get_category_link( $story_category ); ?>" class="category-tag"><?php echo get_cat_name( $story_category ); ?></a>
+				<a href="<?php echo get_category_link( get_sub_field( 'story_category' ) ); ?>" class="category-tag"><?php echo get_cat_name( get_sub_field( 'story_category' ) ); ?></a>
 			</div>
 		<?php endif // pull_latest_from_category ?>
-
 			</div> <!-- secondary-featured-block two in-bottom -->
-		<?php endif // content_enable ?>
+		<?php endif // $content_type === post_list ?>
 	<?php endwhile // have_rows ?>
 <?php endif // have_rows ?>     
 <?php
@@ -121,7 +128,7 @@
 ?>
 <?php if( have_rows('content_3') ): ?>
 	<?php while ( have_rows('content_3') ) : the_row(); ?> 
-		<div class="secondary-featured-block three">
+		<div class="secondary-featured-block three" style="background: transparent;">
 		<?php if( get_sub_field( 'twitter_widget' ) ): ?>
 			<div class="tweet-block" style="background: transparent;">
 				<?php the_sub_field( 'embed_code' ); ?>
---
+++ list-category-posts/secondary-featured.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: List Category Posts - Template "Recent News"
+Description: Template file for List Category Post Plugin for Wordpress which is used by plugin by argument template=value.php
+Version: 0.9
+*/
+
+/**
+* The format for templates changed since version 0.17.  Since this
+* code is included inside CatListDisplayer, $this refers to the
+* instance of CatListDisplayer that called this file.
+*/
+
+/* This is the string which will gather all the information.*/
+$lcp_display_output = '';
+
+// Show category link:
+$lcp_display_output .= $this->get_category_link('strong');
+
+// Show the conditional title:
+$lcp_display_output .= $this->get_conditional_title();
+
+//Add 'starting' tag. Here, I'm using an unordered list (ul) as an example:
+$lcp_display_output .= '<ul class="lcp_catlist">';
+
+/* Posts Loop
+ *
+ * The code here will be executed for every post in the category.  As
+ * you can see, the different options are being called from functions
+ * on the $this variable which is a CatListDisplayer.
+ *
+ * CatListDisplayer has a function for each field we want to show.  So
+ * you'll see get_excerpt, get_thumbnail, etc.  You can now pass an
+ * html tag as a parameter. This tag will sorround the info you want
+ * to display. You can also assign a specific CSS class to each field.
+*/
+
+global $post;
+
+ob_start();
+while ( have_posts() ) : the_post();
+ 	
+	/**
+	 * Display posts, this doesn't change when widget options change
+	 */
+  	echo '<li>';
+	echo '<a href="' . esc_url( get_permalink() ) . '">';
+	if ( has_post_thumbnail() ):
+  		echo '<div class="widget-post-thumbnail" style="background-image:url(' . get_the_post_thumbnail_url() . ');"></div>';
+  	endif;
+
+	echo '<h3 class="lcp_post">';
+	echo the_title();
+	echo '</h3>'; 
+	echo '</a>';
+  	
+	the_date('', '<p class="lcp_date">', '</p>');
+	
+	echo '<div class="lcp_excerpt">';
+		the_excerpt();
+	echo '</div>';
+	echo '</li>';
+	
+endwhile;
+ $lcp_display_output .= ob_get_clean();
+ 
+// Close the wrapper I opened at the beginning:
+$lcp_display_output .= '</ul>';
+
+// If there's a "more link", show it:
+$lcp_display_output .= $this->get_morelink();
+
+// Get category posts count
+$lcp_display_output .= $this->get_category_count();
+
+//Pagination
+$lcp_display_output .= $this->get_pagination();
+
+$this->lcp_output = $lcp_display_output;
+
+
