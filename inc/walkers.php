522ab5b7c3734cb238960f68daeee3e4b9efac75
sezj <sarahzachrich@gmail.com>
2019-12-13 16:02:00 -0500

    Remove inappropriate "menu" and "menuitem" roles from primary nav dropdown menus

--- inc/walkers.php
+++ inc/walkers.php
@@ -6,7 +6,7 @@ class hwcoe_ufl_main_nav_menu extends Walker_Nav_Menu {
 
     public function start_lvl( &$output, $depth = 0, $args = array() ) {
         $indent = str_repeat("\t", $depth);
-		$output .= "\n$indent<div class=\"dropdown\">\n<ul role=\"menu\">";
+		$output .= "\n$indent<div class=\"dropdown\">\n<ul>";
     }
 	
 	public function end_lvl( &$output, $depth = 0, $args = array() ) {
@@ -57,7 +57,6 @@ add_filter( 'nav_menu_item_args', 'hwcoe_ufl_main_menu_item_args', 10, 3);
 function hwcoe_ufl_main_menu_link_attrs( $atts, $item, $args, $depth = null ){
 	if ( ('main_menu' == $args->theme_location) && (0 == $depth) ){
 		$atts['class'] = 'main-menu-link';
-		$atts['role'] = 'menuitem';
 	}
 	return $atts;
 }
@@ -68,7 +67,7 @@ class hwcoe_ufl_main_nav_fallback extends Walker_Page {
 
 	public function start_lvl( &$output, $depth = 0, $args = array() ) {
         $indent = str_repeat("\t", $depth);
-		$output .= "\n$indent<div class=\"dropdown\">\n<ul role=\"menu\">";
+		$output .= "\n$indent<div class=\"dropdown\">\n<ul>";
     }
 	
 	public function end_lvl( &$output, $depth = 0, $args = array() ) {
