107e6404ad8cb1d22bb292f6af9fb8d25739b0ed
sezj <sarahzachrich@gmail.com>
2020-01-09 14:58:25 -0500

    Refactor ufl-category-content module to escape field displays and avoid repetition; add aria-label to links

--- inc/modules/ufl-category-content.php
+++ inc/modules/ufl-category-content.php
@@ -2,19 +2,16 @@
 	$image    = '';
 	$content  = '';
 	$headline = '';
+	$content_box_copy = '';
+	$content_box_img = '';
 	$category_obj	= get_sub_field( 'category' );
 	$category_name = get_cat_name( $category_obj );	
 	$anchor = ( get_sub_field( 'in_page_anchor' ) ? 'id="' . esc_attr( get_sub_field( 'in_page_anchor' ) ) . '"' : '');
 ?>
 <?php if( get_sub_field( 'pull_latest_from_category' ) ): ?>
-	<?
-		/*
-		 * Pull latest story with image
-		 * from specified category
-		 */
-	?>
-	<?php
-		$args = array(
+	<?php 
+	// Pull latest story with image from specified category
+	$args = array(
 		'posts_per_page'   => 10,
    		'category'         => $category_obj, 
 		'orderby'          => 'date',
@@ -44,41 +41,46 @@
 		$headline = get_sub_field( 'headline' );
 	?>
 <?php endif // pull_latest_from_category ?>
+<?php
+	// image content div
+	$content_box_img = '<div class="col-sm-5 content-box-img" style="background-image:url(' . esc_url( $image ). '"></div>';
+
+	// post copy div
+	$content_box_copy .= '<div class="col-sm-7 content-box-copy">';
+	if ( $headline ) {
+		$content_box_copy .= '<h2>' . esc_html( $headline ) . '</h2>';
+	}
+	$content_box_copy .= wp_kses_post( $content );
+	if ( have_rows ( 'read_more_links' ) ):
+		while( have_rows( 'read_more_links' ) ) : the_row();
+			$readmore_link = get_sub_field( 'read_more_url' );
+			$readmore_text = get_sub_field( 'read_more_text' );
+			if ( strtolower( $readmore_text ) == "learn more" || strtolower( $readmore_text ) == "read more")  {
+				$readmore_label = $readmore_text . ": " . $headline;
+			} else {
+				$readmore_label = $readmore_text;
+			}
+			$content_box_copy .= ' <a href="' . esc_url( $readmore_link ) . '" aria-label="' . esc_attr( $readmore_label ) . '" class="read-more">' . esc_html( $readmore_text ) . '</a>';
+		endwhile;
+	endif;
+	$content_box_copy .= '<a href="' . esc_url( get_category_link( $category_obj ) ) . '" class="category-tag orange" aria-label="' . esc_attr($category_name) . '">' . esc_html( $category_name ) . '</a>';
+	$content_box_copy .= '</div>';
+?>
 <!-- ufl-category-content module -->
 <div class="content-box-module" <?php echo $anchor; ?>>
-		<div class="container">
-			<div class="row">
-				<?php if( 'right' == get_sub_field( 'image_position' ) ): ?>
-				<div class="col-sm-7 content-box-copy">
-				<?php if( $headline ): ?>
-				<h2><?php echo esc_html( $headline ); ?></h2>
-				<?php endif; ?>
-					<?php echo wp_kses_post( $content ); ?>
-					<?php if( have_rows ( 'read_more_links' ) ): ?>
-						<?php while( have_rows( 'read_more_links' ) ) : the_row(); ?>
-							<a href="<?php esc_url( the_sub_field( 'read_more_url' ) ); ?>" class="read-more"><?php esc_html_e( the_sub_field( 'read_more_text' ) ); ?></a>
-							<?php endwhile // have_rows ?>
-						<?php endif // have_rows ?>
-							<a href="<?php echo esc_url( get_category_link( $category_obj ) ); ?>" class="category-tag orange"><?php echo esc_html( $category_name ); ?></a>
-				</div>
-				<div class="col-sm-5 content-box-img" style="background-image:url(<?php echo esc_url( $image ); ?>)">
-				</div>
-				<?php elseif( 'left' == get_sub_field( 'image_position' ) ): ?>
-				 <div class="col-sm-5 content-box-img" style="background-image:url(<?php echo esc_url( $image ); ?>)">
-				</div>
-				<div class="col-sm-7 content-box-copy">
-				<?php if( $headline ): ?>
-				<h2><?php echo esc_url($headline); ?></h2>
-				<?php endif ?>
-					<?php echo wp_kses_post( $content ); ?>
-					<?php if( have_rows ( 'read_more_links' ) ): ?>
-						<?php while( have_rows( 'read_more_links' ) ) : the_row(); ?>
-							<a href="<?php esc_url(the_sub_field( 'read_more_url' )); ?>" class="read-more"><?php esc_html(the_sub_field( 'read_more_text' )); ?></a>
-							<?php endwhile // have_rows ?>
-						<?php endif // have_rows ?>
-							<a href="<?php echo esc_url(get_category_link( $category_obj )); ?>" class="category-tag orange"><?php echo esc_html($category_name); ?></a>
-				</div>
-				<?php endif // image_position ?>
-			</div>
+	<div class="container">
+		<div class="row">
+			<?php if( 'right' == get_sub_field( 'image_position' ) ): ?>
+				<?php 
+					echo $content_box_copy;
+					echo $content_box_img;
+				?>
+			<?php elseif( 'left' == get_sub_field( 'image_position' ) ): ?>
+				<?php 
+					echo $content_box_img;
+					echo $content_box_copy;
+				?>
+			<?php endif // image_position ?>
 		</div>
 	</div>
+</div>
