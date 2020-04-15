3bc6e7ddbe766c9fbaaaef6c61b005a22f7d53d1
sezj <sarahzachrich@gmail.com>
2020-04-15 12:20:27 -0400

    Update Staff Listing custom field group to allow section headings and multiple instances of a module per page

--- page-templates/staff.php
+++ page-templates/staff.php
@@ -9,31 +9,28 @@
 get_header(); ?>
 	
 <div id="main" class="container main-content">
-<div class="row">
-  <div class="col-sm-12">
-		<?php hwcoe_ufl_breadcrumbs(); ?>
-		<header class="entry-header" aria-label="Content Header">
-			<?php hwcoe_ufl_entry_title(); ?>
-		</header>
-  </div>
-</div>
-<div class="row">
+	<div class="row">
+		<div class="col-sm-12">
+			<?php hwcoe_ufl_breadcrumbs(); ?>
+			<header class="entry-header" aria-label="Content Header">
+				<?php hwcoe_ufl_entry_title(); ?>
+			</header>
+		</div>
+	</div>
+	<div class="row">
   
-  <?php get_sidebar('page_sidebar'); ?>  
+	<?php get_sidebar('page_sidebar'); ?>  
   
-  <div class="<?php echo hwcoe_ufl_page_column_class(); ?>">
-  	<!-- repeat page title for screen reader users who skip nav -->
-  	<span class="visuallyhidden"><?php hwcoe_ufl_entry_title(); ?></span>
-	 <?php 
-		while ( have_posts() ) : the_post();
-			get_template_part( 'template-parts/content', 'staff' );
-		endwhile; // End of the loop. 
-	?>
-  </div>
+		<div class="<?php echo hwcoe_ufl_page_column_class(); ?>">
+		<?php 
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', 'staff' );
+			endwhile; // End of the loop. 
+		?>
+		</div>
+   <?php get_sidebar('page_right'); ?> 
   
-  <?php get_sidebar('page_right'); ?> 
-  
-</div><!-- .row -->
+	</div><!-- .row -->
 </div><!-- #main -->
 
 <?php get_footer(); ?>
--- template-parts/content-staff.php
+++ template-parts/content-staff.php
@@ -16,42 +16,54 @@
 		
 		<?php if( have_rows('staff_listing') ): ?>
 			<?php while( have_rows('staff_listing') ): the_row(); ?>
-			<?php
-				$staff_name				= get_sub_field('staff_name');
-				$staff_url				= ( get_sub_field('staff_url') ? get_sub_field('staff_url') : '');
-				$staff_title			= get_sub_field('staff_title');
-				$staff_description	= ( get_sub_field('staff_description') ? get_sub_field('staff_description') : '' );
-				if ( get_sub_field('staff_photo') ) {
-					$staff_photo_url	= get_sub_field('staff_photo')['url'];
-				} else {
-					$staff_photo_url	= '';
-				}
-			?>
-			<h3 class="member-name entry-title">
-			<?php if($staff_url != ''): ?>
-				<a href="<?php echo esc_url($staff_url); ?>"><?php echo esc_html($staff_name); ?></a>
-			<?php else: ?>
-				<?php echo esc_html($staff_name); ?>
-			<?php endif; // $staff_url ?>
-			</h3>
-			
-			<div class="row">
-			<?php if ($staff_photo_url != ''): ?>
-				<div class="col-md-3">			
-					<img src="<?php echo esc_url($staff_photo_url); ?>" alt="<?php echo esc_attr($staff_name); ?>" width="180" />
-				</div>
-				<div class="col-md-9">
-			<?php else: ?>
-				<div class="col-md-12">
-			<?php endif; // $staff_photo_url ?>
-					<h4><?php echo esc_html($staff_title); ?></h4>
-					<?php if($staff_description != ''): ?>
-					<?php echo wpautop( wp_kses_post($staff_description) ); ?>
-					<?php endif; // $staff_description ?>
-				</div>
-			</div> <!-- row -->
-			<hr style="margin:20px 0 30px;" />
-			<!-- <hr /> -->
+				<?php if( get_row_layout() == 'staff_listing_section' ): ?>
+					<?php 
+						$section_heading = ( get_sub_field('staff_section_heading') ? get_sub_field('staff_section_heading') : ''); 
+						if ($section_heading != '') {
+							echo "<h2>" . esc_html($section_heading) . "</h2>";
+						}
+					?>
+					<?php if( have_rows ( 'staff_member' ) ): ?>
+						<?php while( have_rows( 'staff_member' ) ) : the_row(); ?>
+							<?php 
+								$staff_name				= get_sub_field('staff_name');
+								$staff_url				= ( get_sub_field('staff_url') ? get_sub_field('staff_url') : '');
+								$staff_title			= get_sub_field('staff_title');
+								$staff_description	= ( get_sub_field('staff_description') ? get_sub_field('staff_description') : '' );
+								if ( get_sub_field('staff_photo') ) {
+									$staff_photo_url	= get_sub_field('staff_photo')['url'];
+								} else {
+									$staff_photo_url	= '';
+								}
+							?>
+							<h3 class="member-name entry-title">
+								<?php if($staff_url != ''): ?>
+								<a href="<?php echo esc_url($staff_url); ?>"><?php echo esc_html($staff_name); ?></a>
+								<?php else: ?>
+									<?php echo esc_html($staff_name); ?>
+								<?php endif; // $staff_url ?>
+							</h3>
+							<div class="row">
+							<?php if ($staff_photo_url != ''): ?>
+								<div class="col-md-3">			
+									<img src="<?php echo esc_url($staff_photo_url); ?>" alt="<?php echo esc_attr($staff_name); ?>" style="max-width:100%;" />
+								</div>
+								<div class="col-md-9">
+							<?php else: ?>
+								<div class="col-md-12">
+							<?php endif; // $staff_photo_url ?>
+									<h4><?php echo esc_html($staff_title); ?></h4>
+									<?php if($staff_description != '') {
+										echo wpautop( wp_kses_post($staff_description) );
+									} ?>
+								</div>
+							</div> <!-- row -->
+							<hr style="margin:20px 0 30px;" />
+
+							<?php endwhile // the_row staff_member ?>
+						<?php endif // have_rows staff_member ?>
+					
+				<?php endif; // get_row_layout ?>
 			<?php endwhile; //the_row ?>
 		<?php endif; // have_rows ?>
 	</div><!-- .entry-content -->
