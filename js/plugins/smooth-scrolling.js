daf4d0e5832dca2ba4a71102410633ae77eb1371
sezj <sarahzachrich@gmail.com>
2023-10-20 09:40:19 -0400

    Add offset for top menu to anchor links with different page destination

--- js/plugins/smooth-scrolling.js
+++ js/plugins/smooth-scrolling.js
@@ -9,29 +9,41 @@ jQuery(function($) {
 	.not('[href="#"]')
 	.not('[href="#0"]')
 	.click(function(event) {
-	// On-page links
-	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-		// Figure out element to scroll to
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		// Does a scroll target exist?
-		if (target.length) {
-			// Only prevent default if animation is actually gonna happen
-			event.preventDefault();
+		// On-page links
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
 
-			// Animate scrolling
-			$("html, body").animate({ scrollTop: target.offset().top - 260}, 500, "linear");
-
-			// set focus in new location
-			target.focus();
-			if (target.is(":focus")){
-				return false;
-			} else {
-				target.attr("tabindex","-1");
+				// Animate scrolling
+				$("html, body").animate({ scrollTop: target.offset().top - 240}, 500, "linear");
+				
+				// set focus in new location
 				target.focus();
+				if (target.is(":focus")){
+					return false;
+				} else {
+					target.attr("tabindex","-1");
+					target.focus();
+				}
+				return false;
 			}
-			return false;
-		}
-	 }
-  });
+	 	}
+  	});
 });
+
+// different-page links
+jQuery ( document ).ready ( function($) {
+var hash= window.location.hash
+if ( hash == '' || hash == '#' || hash == undefined ) return false;
+      var target = $(hash);
+      target = target.length ? target : $('[name=' + hash.slice(1) +']');
+      if (target.length) {
+		// Animate scrolling
+        $('html,body').stop().animate({ scrollTop: target.offset().top - 240}, 500, 'linear');
+      }
+} );
